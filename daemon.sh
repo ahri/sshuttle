ce96110c5f5099d1d45091b4d1b3a3ec0ed7b82d
Adam Piper <adam.piper@logica.com>
2011-08-23 15:04:02 +0100

    Give some info about hostname before and after daemon start

--- daemon.sh
+++ daemon.sh
@@ -5,4 +5,13 @@ remote="$1"
 set -eu
 shift
 
+print_host()
+{
+        title="$1"
+        printf "Hostname $title: %s\n" $(curl -q -s --noproxy '*' http://ifconfig.me/host)
+}
+
+print_host before
 $(dirname "$0")/sshuttle -x 10.0.0.0/8 -x 192.168.0.0/16 -r "$remote" -D "$@" 0/0
+sleep 5
+print_host after
