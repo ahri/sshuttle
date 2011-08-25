757703d978f18a39e77f3d0ed8407c0a323d139f
Adam Piper <adam.piper@logica.com>
2011-08-25 13:48:03 +0100

    Neaten very slightly... pickiness.......

--- watcher.sh
+++ watcher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 . "$(dirname "$0")/daemon.sh" "$@"
-echo "Hit enter to kill off the connection"
+printf "Hit enter to kill off the connection..."
 read _
 kill "$(cat "$(dirname "$0")/sshuttle.pid")"
 sleep 5
