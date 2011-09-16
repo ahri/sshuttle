f2f4f33e05d1802cc74b3e7523a02c2eac3569cd
Adam Piper <adam@ahri.net>
2011-09-16 09:53:19 +0100

    Make it a bash script for read's timeout

--- watcher.sh
+++ watcher.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 pidfile="$(dirname "$0")/sshuttle.pid"
 . "$(dirname "$0")/daemon.sh" "$@"
 printf "Hit enter to kill off the connection..."
 while [[ -f $pidfile ]]; do
     read -t 1 && kill "$(cat "$pidfile")" && break
 done
+echo
 sleep 5
 print_host final
