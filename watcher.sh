b754c910100fc490108f4ac346477c1f92a87f07
Adam Piper <adam@ahri.net>
2011-09-16 09:31:11 +0100

    Deal with possibility that the tunnel gets killed off elsewhere

--- watcher.sh
+++ watcher.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+pidfile="$(dirname "$0")/sshuttle.pid"
 . "$(dirname "$0")/daemon.sh" "$@"
 printf "Hit enter to kill off the connection..."
-read _
-kill "$(cat "$(dirname "$0")/sshuttle.pid")"
+while [[ -f $pidfile ]]; do
+    read -t 1 && kill "$(cat "$pidfile")" && break
+done
 sleep 5
 print_host final
